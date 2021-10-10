6e9d685c85bc8fbef3b44d231aa9e6a7cf326414
Altukhov-Vladislav <90462643+Altukhov-Vladislav@users.noreply.github.com>
2021-10-10 12:03:59 +0300

    Create vector_of_vectors.cpp

---
+++ Useful things/vector_of_vectors.cpp
@@ -0,0 +1,12 @@
+ float x;
+      vector<float> cc(stroki);
+    for (int i = 0; i < stroki; i++) {
+     
+        cc.push_back(x);
+    
+        for (int j = 0; j < stolb; j++) {
+
+            tab.push_back(cc);
+
+        }
+    }
